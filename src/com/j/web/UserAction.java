89191523b5f242ec48114c22aae3587878f3247f
jifanxi <5370807@qq.com>
2016-12-12 17:49:03 +0800

    xxx

--- src/com/j/web/UserAction.java
+++ src/com/j/web/UserAction.java
@@ -4,6 +4,7 @@ public class UserAction {
 	public static void main(String[] args) {
 		System.out.println("HAHA ...");
 		System.out.println("HAHA ...");
+		System.out.println("HAHA ...");
 	}
 	
 	public String login() {
