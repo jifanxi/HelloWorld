e1243bba4dddf79ed0eb60484fc3d07fef488290
jifanxi <5370807@qq.com>
2016-12-12 14:30:41 +0800

    login 3 commit ...

--- src/com/j/web/UserAction.java
+++ src/com/j/web/UserAction.java
@@ -2,12 +2,6 @@ package com.j.web;
 
 public class UserAction {
 	public static void main(String[] args) {
-		System.out.println(1223233);
-		System.out.println(1223233); 
-		System.out.println(1223233);
-		System.out.println(1223233);
-		System.out.println(1223233);
-		System.out.println(1223233);
 		System.out.println("HAHA ...");
 		System.out.println("HAHA ...");
 	}
